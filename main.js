05d3ccefd56b2f682b89010cb515428c84012c5a
Jpeyton <jpwalk319@gmail.com>
2021-01-27 18:38:53 -0700

    put key variable inside the created function

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //This file is the JS for the search page AND results page due to common functions/variables needing to be referenced
-let key = 'dQsyvNyQ9qRp3zhGJhVSf70Yc7utHj2eyGPZXEZ7VNAt1C8bmtH2cVFWQKLoS58Q';
+
 let creatorQry = '&creator=';
 function getWaltersData(query) {
-    
+    let key = 'dQsyvNyQ9qRp3zhGJhVSf70Yc7utHj2eyGPZXEZ7VNAt1C8bmtH2cVFWQKLoS58Q';
     let queryURL = 'http://api.thewalters.org/v1/objects?apikey=' + key + '&title=' + query;
 
     $.ajax({
