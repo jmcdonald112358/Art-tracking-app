7972732dba51e1a8ce2d1da4149c4f6e94f8f581
Jpeyton <jpwalk319@gmail.com>
2021-01-28 16:28:09 -0700

    added a userLimit variable to be set equal to the value returned from its respective form field input. added a for loop into the Walters data function to iterate throught the results as many times as userLimit, updated the variable in for loop within getMetData function as well

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
 //This file is the JS for the search page AND results page due to common functions/variables needing to be referenced
 
-//Function for Walters Collection search by author
 
+
+
+//Function for Walters Collection search by author
 
 function getWaltersData(query) {
     let key = 'dQsyvNyQ9qRp3zhGJhVSf70Yc7utHj2eyGPZXEZ7VNAt1C8bmtH2cVFWQKLoS58Q';
     let queryURL = 'http://api.thewalters.org/v1/objects?apikey=' + key + '&creator=' + query;
-
+    let userLimit = $('#userLimit').val();  
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
-        
-      console.log(response);
+      for (let i = 0; i < userLimit; i++) {
+         console.log(response);
+      } 
+      
     });
 };
 
 //Function for The Met collection
 function getMetData(query) {
    let queryURL = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=" + query;
-   
+   let userLimit = $('#userLimit').val();
+
    $.ajax({
       url: queryURL,
       method: "GET"
    }).then(function(queryResponse) {
       console.log(queryResponse);
       
-      for (i=0; i < 5; i++){ //Update index limiter to reference input req
+      for (i=0; i < userLimit; i++){ //Update index limiter to reference input req
          let objectID = queryResponse.objectIDs[i];
          let objectURL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + objectID; 
 
