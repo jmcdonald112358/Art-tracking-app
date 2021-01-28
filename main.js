d971ecf92b73a3863a8e1ff5997f3d504f5df740
Jpeyton <jpwalk319@gmail.com>
2021-01-27 18:21:44 -0700

    added function to main.js for searching the walters art museum api

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 //This file is the JS for the search page AND results page due to common functions/variables needing to be referenced
+let key = 'dQsyvNyQ9qRp3zhGJhVSf70Yc7utHj2eyGPZXEZ7VNAt1C8bmtH2cVFWQKLoS58Q';
+let creatorQry = '&creator=';
+function getWaltersData(query) {
+    
+    let queryURL = 'http://api.thewalters.org/v1/objects?apikey=' + key + '&title=' + query;
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+    });
+};
+
